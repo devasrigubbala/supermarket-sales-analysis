b8c3808dcac9202c6aaf521ec258d183ef2065de
devasrigubbala <devasrigubbala@gmail.com>
2026-09-21 14:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,14 @@ else:
         st.info(
             f"🔎 Showing {len(df):,} records"
         )
+filtered_df = df.copy()
 
+if search_product:
+    filtered_df = filtered_df[
+        filtered_df["Product Name"].astype(str).str.contains(
+            search_product, case=False, na=False
+        )
+    ]
         # -------- QUICK ANALYSIS --------
 
         col1, col2 = st.columns(2)
